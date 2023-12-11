042e6e0bc5040cd4512bac03a10418960c915c7d
supersudouser <supersudousr@gmail.com>
2023-12-11 14:48:43 +0100

    changed js file to jsx

---
+++ src/piper_website_frontend/src/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { custom_greeting_backend } from "../../declarations/custom_greeting_backend";
+
+
+  return (
+    <div style={{ "fontSize": "30px" }}>
+      Hello
+    </div>
+  );
+
+
+render(<MyHello />, document.getElementById("app"));
