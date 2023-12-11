fac52265bbceb7918427ec3c73a7eb92b40f65e8
supersudouser <supersudousr@gmail.com>
2023-12-11 18:52:26 +0100

    Updated index.js

--- src/piper_website_frontend/src/index.jsx
+++ src/piper_website_frontend/src/index.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { createRoot } from 'react-dom/client';
+import App from "./App";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
-// Render your React component instead
+// Render your React component insteads
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<h1><App/></h1>);
 
 
