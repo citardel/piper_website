24e0179737504f096b2b7a6eca18fc178faa11d2
supersudouser <supersudousr@gmail.com>
2023-12-12 23:04:02 +0100

    Added changes

--- src/piper_website_frontend/src/App.jsx
+++ src/piper_website_frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "./component/Header";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div className = "App">
       <Header/>
+      <Home/>
     </div>
   )
 }
